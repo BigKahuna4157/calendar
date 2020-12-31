36b536eb847c82c4fcbe352d5872d9ebdb93c0c2
ahmad <ahmad.moghrabi@github.com>
2020-12-30 19:02:03 -0500

    Naif's Calendar

--- src/ca/ashbury/naif/calendar/Controller.java
+++ src/ca/ashbury/naif/calendar/Controller.java
@@ -1,21 +1,28 @@
 package ca.ashbury.naif.calendar;
 
+import java.net.URL;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.util.converter.LocalDateTimeStringConverter;
 
-public class Controller {
+public class Controller implements Initializable {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     private Calendar calendar;
 
     @FXML
-    TableView tvEvents;
+    TableView<Event> tvEvents;
 
     @FXML
     TextField tfTime;
@@ -51,23 +58,40 @@ public class Controller {
         calendar = Calendar.getInstance();
     }
 
-    @FXML
-    private void createEvent(ActionEvent event) {
-        event.consume();
-        calendar.createEvent(tfName.getText(), LocalDateTime.parse(tfTime.getText().replace(" ", "T")), taLocation.getText(), taNote.getText());
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadEvents();
+        time.setCellFactory(TextFieldTableCell.forTableColumn(new LocalDateTimeStringConverter()));
+        name.setCellFactory(TextFieldTableCell.forTableColumn());
+        address.setCellFactory(TextFieldTableCell.forTableColumn());
+        note.setCellFactory(TextFieldTableCell.forTableColumn());
     }
 
-    @FXML
-    private void loadEvents(ActionEvent event) {
-        event.consume();
-        id.setCellValueFactory(new PropertyValueFactory<>("id"));
-        name.setCellValueFactory(new PropertyValueFactory<>("name"));
-        time.setCellValueFactory(new PropertyValueFactory<>("time"));
-        address.setCellValueFactory(new PropertyValueFactory<>("location"));
-        note.setCellValueFactory(new PropertyValueFactory<>("note"));
+    private void loadEvents() {
+        tvEvents.getItems().clear();
         for (Event lEvent : calendar.getEvents()) {
             tvEvents.getItems().add(lEvent);
         }
+    }
+
+    @FXML
+    private void createEvent(ActionEvent event) {
+        event.consume();
+        calendar.createEvent(tfName.getText(), LocalDateTime.parse(tfTime.getText(), formatter), taLocation.getText(), taNote.getText());
+        loadEvents();
+    }
+
+    @FXML
+    private void modifyEvent(ActionEvent event) {
+        event.consume();
+    }
+
+    @FXML
+    private void deleteEvent(ActionEvent event) {
+        event.consume();
+        Event levent = tvEvents.getSelectionModel().getSelectedItem();
+        calendar.deleteEvent(levent.getId());
+        loadEvents();
     }
 
 }
